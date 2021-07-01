c780a3c7c647afabf41a0ac02763a3473e6d44e6
Haoyuan Li <vamirio@qq.com>
2021-07-01 15:44:13 +0800

    Add class doubly_linked_list

--- template/list.hpp
+++ template/list.hpp
@@ -1,6 +1,7 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+
 #include <iostream>
 
 template <typename T>
@@ -18,7 +19,7 @@ class singly_linked_list {
                 bool is_tail();
                 sll *find_prev_node(const sll *const head);
                 sll *find_prev_node(const sll *const head, const T &elem);
-                sll *find_node(const sll *const head, const T &elem);
+                sll *find(const T &elem);
                 sll *insert(const sll *const head, sll *const node);
                 sll *insert(const sll *const head, const T &elem);
                 sll *append(sll *const node);
@@ -27,11 +28,42 @@ class singly_linked_list {
                 sll *remove(const sll *const head);
                 sll *remove(const sll *const head, const T &elem);
                 sll *clear();
-                sll *clear(sll *const node);
                 void print();
 };
 
 
+template <typename T>
+class doubly_linked_list {
+        private:
+                typedef doubly_linked_list<T> dll;
+        public:
+                T elem;
+                dll *prev;
+                dll *next;
+
+        public:
+                doubly_linked_list();
+                doubly_linked_list(const T &elem);
+                doubly_linked_list(const T &elem, const dll *const prev,
+                                const dll *const next);
+                bool is_head();
+                bool is_tail();
+                dll *find(const T &elem);
+                dll *rfind(const T &elem);
+                dll *insert(dll *const node);
+                dll *insert(const T &elem);
+                dll *append(dll *const node);
+                dll *append(const T &elem);
+                dll *remove();
+                dll *clear();
+                void print();
+};
+
+
+/*-------------------------------------------------------
+ * singly_linked_list
+ *-------------------------------------------------------
+ */
 template <typename T>
 singly_linked_list<T>::singly_linked_list()
 {
@@ -107,16 +139,15 @@ singly_linked_list<T> *singly_linked_list<T>::find_prev_node(
 
 
 /*-------------------------------------------------------
- * find_node - find the first node with element @elem,
- * return the pointer point to the node found, NULL if
- * not found
+ * find - find the first node with element @elem
+ * after this node, return the pointer point to the node
+ * found, NULL if not found
  *-------------------------------------------------------
  */
 template <typename T>
-singly_linked_list<T> *singly_linked_list<T>::find_node(
-                const sll *const head, const T &elem)
+singly_linked_list<T> *singly_linked_list<T>::find(const T &elem)
 {
-        sll *p = head->next;
+        sll *p = this->next;
         while (p && p->elem != elem)
                 p = p->next;
         return p;
@@ -132,12 +163,11 @@ template <typename T>
 singly_linked_list<T> *singly_linked_list<T>::insert(
                 const sll *const head, sll *const node)
 {
+        sll *p = NULL;
         sll *prev = this->find_prev_node(head);
-        if (prev == NULL)
-                return NULL;
-        prev->next = node;
-        node->next = this;
-        return node;
+        if (prev)
+                p = prev->append(node);
+        return p;
 }
 
 
@@ -151,12 +181,11 @@ template <typename T>
 singly_linked_list<T> *singly_linked_list<T>::insert(
                 const sll * const head, const T &elem)
 {
+        sll *p = NULL;
         sll *prev = this->find_prev_node(head);
-        sll *node = new (std::nothrow) singly_linked_list<T>(elem, this);
-        if (!prev || !node)
-                return NULL;
-        prev->next = node;
-        return node;
+        if (prev)
+                p = prev->append(elem);
+        return p;
 }
 
 
@@ -184,9 +213,8 @@ template <typename T>
 singly_linked_list<T> *singly_linked_list<T>::append(const T &elem)
 {
         sll *p = new (std::nothrow) singly_linked_list<T>(elem, this->next);
-        if (!p)
-                return NULL;
-        this->next = p;
+        if (p)
+                this->next = p;
         return p;
 }
 
@@ -253,26 +281,14 @@ singly_linked_list<T> *singly_linked_list<T>::remove(
 template <typename T>
 singly_linked_list<T> *singly_linked_list<T>::clear()
 {
-        return this->clear(this);
+        while (!this->is_tail())
+                this->remove();
+        return this;
 }
 
 
 /*-------------------------------------------------------
- * clear - clear all nodes behind the node @node, return
- * the pointer point to @node
- *-------------------------------------------------------
- */
-template <typename T>
-singly_linked_list<T> *singly_linked_list<T>::clear(sll *const node)
-{
-        while (!node->is_tail())
-                node->remove();
-        return node;
-}
-
-
-/*-------------------------------------------------------
- * print - print the list
+ * print - print all nodes after this node
  *-------------------------------------------------------
  */
 template <typename T>
@@ -285,5 +301,212 @@ void singly_linked_list<T>::print()
         }
         std::cout << std::endl;
 }
+
+
+/*-------------------------------------------------------
+ * doubly_linked_list
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T>::doubly_linked_list()
+{
+        this->elem = T();
+        this->prev = NULL;
+        this->next = NULL;
+}
+
+
+template <typename T>
+doubly_linked_list<T>::doubly_linked_list(const T &elem)
+{
+        this->elem = elem;
+        this->prev = NULL;
+        this->next = NULL;
+}
+
+
+template <typename T>
+doubly_linked_list<T>::doubly_linked_list(const T &elem, const dll *const prev,
+                const dll *const next)
+{
+        this->elem = elem;
+        this->prev = const_cast<dll *>(prev);
+        this->next = const_cast<dll *>(next);
+}
+
+
+/*-------------------------------------------------------
+ * is_head - check whether this node is the head node
+ *-------------------------------------------------------
+ */
+template <typename T>
+bool doubly_linked_list<T>::is_head()
+{
+        return !this->prev;
+}
+
+
+/*-------------------------------------------------------
+ * is_tail - check whether this node is the last node
+ *-------------------------------------------------------
+ */
+template <typename T>
+bool doubly_linked_list<T>::is_tail()
+{
+        return !this->next;
+}
+
+
+/*-------------------------------------------------------
+ * find - find the first node with element @elem after
+ * this node, return the pointer point to the node found,
+ * NULL if not found
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::find(const T &elem)
+{
+        dll *p = this->next;
+        while (p && p->elem != elem)
+                p = p->next;
+        return p;
+}
+
+
+/*-------------------------------------------------------
+ * rfind - inverse find the first node with the element
+ * @elem, return the pointer point to the node found,
+ * NULL if not found
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::rfind(const T &elem)
+{
+        if (this->is_head())
+                return NULL;
+        dll *p = this->prev;
+        while (!p->is_head() && p->elem != elem)
+                p = p->prev;
+        if (p->is_head())
+                p = NULL;
+        return p;
+}
+
+
+/*-------------------------------------------------------
+ * insert - insert the node @node before this node,
+ * return the pointer point to @node, NULL if failed
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::insert(dll *const node)
+{
+        if (this->is_head())
+                return NULL;
+        this->prev->append(node);
+        return node;
+}
+
+
+/*-------------------------------------------------------
+ * insert - insert a new node with element @elem before
+ * this node, return the pointer point to the new node,
+ * NULL if failed
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::insert(const T &elem)
+{
+        if (this->is_head())
+                return NULL;
+        dll *p = this->prev->append(elem);
+        return p;
+}
+
+
+/*-------------------------------------------------------
+ * append - append the node @node after this node, return
+ * the pointer point to @node
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::append(dll *const node)
+{
+        if (!this->is_tail())
+                this->next->prev = node;
+        node->prev = this;
+        node->next = this->next;
+        this->next = node;
+        return node;
+}
+
+
+/*-------------------------------------------------------
+ * append - append a new node with element @elem to this
+ * node, return the pointer point to the new node, NULL
+ * if failed
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::append(const T &elem)
+{
+        dll *p = new (std::nothrow) dll(elem, this, this->next);
+        if (p) {
+                if (!this->is_tail())
+                        this->next->prev = p;
+                this->next = p;
+        }
+        return p;
+}
+
+
+/*-------------------------------------------------------
+ * remove - remove this node, return the poniter point to
+ * the next node
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::remove()
+{
+        if (this->is_head())    /* it's illegal to remove a head node */
+                return NULL;
+        dll *p = this->next;
+        if (!this->is_tail())
+                p->prev = this->prev;
+        this->prev->next = this->next;
+        delete this;
+        return p;
+}
+
+
+/*-------------------------------------------------------
+ * clear - clear all nodes behind this node, return the
+ * pointer point to this node
+ *-------------------------------------------------------
+ */
+template <typename T>
+doubly_linked_list<T> *doubly_linked_list<T>::clear()
+{
+        while (!this->is_tail())
+                this->next->remove();
+        return this;
+}
+
+
+/*-------------------------------------------------------
+ * print - print all nodes after this node
+ *-------------------------------------------------------
+ */
+template <typename T>
+void doubly_linked_list<T>::print()
+{
+        dll *p = this->next;
+        while (p) {
+                std::cout << p->elem << " ";
+                p = p->next;
+        }
+        std::cout << "\n";
+}
+
 
 #endif /* LIST_HPP */
--- template/test_list.cpp
+++ template/test_list.cpp
@@ -2,7 +2,7 @@
  * test_list.cpp - test the class list
  *
  * Created by Haoyuan Li on 2021/06/30
- * Last Modified: 2021/07/01 09:46:09
+ * Last Modified: 2021/07/01 15:42:27
  *=======================================================
  */
 
@@ -11,8 +11,13 @@
 #include "list.hpp"
 
 typedef singly_linked_list<int> sll;
-int main()
+typedef doubly_linked_list<int> dll;
+
+
+void test_sll()
 {
+        std::cout << "Start testing class singly_linked_list<int>:"
+                << std::endl;
         sll *head = new sll();
         assert(1 == head->is_tail());
         sll *p = head->append(3);
@@ -25,7 +30,9 @@ int main()
         p = (p->find_prev_node(head, 3))->remove();
         std::cout << "Execpted output: 1 0 2\nActual output:   ";
         head->print();
-        p = (p->find_node(head, 0))->remove(head);
+        p = head->find(2);
+        assert(2 == p->elem);
+        p = (head->find(0))->remove(head);
         std::cout << "Execpted output: 1 2\nActual output:   ";
         head->print();
         p = (p->find_prev_node(head))->append(3);
@@ -34,11 +41,45 @@ int main()
         p = p->remove(head, 3);
         std::cout << "Execpted output: 1 2\nActual output:   ";
         head->print();
-        p = p->clear(p->find_node(head, 1));
-        std::cout << "Execpted output: 1\nActual output:   ";
-        head->print();
         head->clear();
         assert(1 == head->is_tail());
+        delete head;
+        std::cout << "End testing." << std::endl;
+}
+
+
+void test_dll()
+{
+        std::cout << "Start testing class doubly_linked_list<int>:"
+                << std::endl;
+        dll *head = new dll();
+        dll *p;
+        assert(1 == head->is_head());
+        assert(1 == head->is_tail());
+        p = head->append(2);
+        p = p->append(new dll(3, p, p->next));
+        p = (head->find(2))->insert(1);
+        p = p->insert(new dll(0));
+        std::cout << "Execpted output: 0 1 2 3\nActual output:   ";
+        head->print();
+
+        p = (head->find(3))->rfind(1);
+        assert(1 == p->elem);
+        p = p->remove();
+        std::cout << "Execpted output: 0 2 3\nActual output:   ";
+        head->print();
+        p = head->clear();
+        assert(1 == head->is_tail());
+        delete head;
+        std::cout << "End testing." << std::endl;
+}
+
+
+int main()
+{
+        test_sll();
+        std::cout << std::endl;
+        test_dll();
 
         return 0;
 }
