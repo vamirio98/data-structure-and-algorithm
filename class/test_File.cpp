df3ac72eb94d0e24acd5406f94b6c13925f6f6ec
Haoyuan Li <vamirio@qq.com>
2021-08-16 13:22:25 +0800

    update test file

--- class/test_File.cpp
+++ class/test_File.cpp
@@ -2,7 +2,7 @@
  * test_File.cpp - test the File class
  *
  * Created by Haoyuan Li on 2021/08/11
- * Last Modified: 2021/08/15 23:12:19
+ * Last Modified: 2021/08/16 13:21:35
  */
 
 #include "File.hpp"
@@ -16,109 +16,137 @@ using std::endl;
 
 using std::string;
 
-int main()
+void ls(const string &arg)
 {
 #if defined(__unix__)
-        File file1("./hello/world");
-        File file2("../class", "test_File.cpp");
-        File file3(".bashrc");
-        assert(file1.get_parent() == "./hello");
+        system((string{"ls -l "} + arg).c_str());
 #elif defined(_MSC_VER)
-        File file1(".\\hello\\world");
-        File file2("..\\class", "test_File.cpp");
-        assert(file1.get_parent() == ".\\hello");
+        system((string{"dir "} + arg).c_str());
+#endif
+}
+
+int main(int argc, char **argv)
+{
+        string s1;
+        string ts1;
+        string s2{"test_File.cpp"};
+        string s3;
+#if defined(__unix__)
+        s1 = "./hello/world";
+        ts1 = "./hello";
+        s3 = "~/.bashrc";
+        File f2("../class", s2);
+#elif defined(_MSC_VER)
+        s1 = ".\\hello\\world";
+        ts1 = ".\\hello";
+        s2 = "..\\.git";
+        File f2("..\\class", s2);
 #endif
 
-        assert(!file1.exists());
-        assert(file2.exists());
-        assert(file2.is_file());
-        assert(!file2.is_directory());
-        assert(File{file2.get_parent()}.is_directory());
-        assert(!file2.is_hidden());
+        File f1(s1);
+        assert(f1.get_parent() == ts1);
+        assert(!f1.exists());
+        assert(f2.exists());
+        assert(f2.is_file());
+        assert(!f2.is_directory());
+        assert(File{f2.get_parent()}.is_directory());
+        assert(!f2.is_hidden());
+
+        File f3(s3);
+        assert(f3.is_hidden());
+
+        assert(f1.get_name() == "world");
+        f1.unbind();
+        assert(f1.get_name() == "");
+        f1.bind("./hello/world");
+        assert(f1.get_path() == s1);
+
+        cout << "The absolute path of " << f1.get_path() << " is " <<
+                f1.get_absolute_path() << endl << endl;
+        cout << "The absolute path of " << f2.get_path() << " is " <<
+                f2.get_absolute_path() << endl << endl;
+
+        cout << "The last modified time of " << f2.get_path() << " is " <<
+                File::get_time_str(f2.last_modified()) << endl << endl;
+
+        cout << "The size of " << f1.get_path() << " is " << f1.get_size()
+                << " bytes" << endl << endl;
+        cout << "The size of " << f2.get_path() << " is " << f2.get_size()
+                << " bytes" << endl << endl;
+
+        assert(!f1.can_read());
+        assert(!f1.can_write());
+        assert(!f1.can_execute());
+        assert(f2.can_read());
+        assert(f2.can_write());
+        assert(File(argv[0]).can_execute());
+        assert(File(f2.get_parent()).can_execute());
+
+        cout << "Expected: nothing" << endl;
+        for (const auto &s : f1.list())
+                cout << s << " ";
+        cout << endl << endl;
+
+        cout << "Expected: nothing" << endl;
+        for (const auto &s : f2.list())
+                cout << s << " ";
+        cout << endl << endl;
+
+        cout << "Expected: all files in this directory" << endl;
+        for (const auto &s : File(f2.get_parent()).list())
+                cout << s << " ";
+        cout << endl << endl;
+
+        assert(f1.get_extension() == "");
+        assert(f2.get_extension() == "cpp");
+
+        string s4;
 #if defined(__unix__)
-        assert(file3.is_hidden());
+        s4 = "./hello";
+#elif defined(_MSC_VER)
+        s3 = ".\\hello";
 #endif
 
-        assert(file1.get_name() == "world");
-        file1.unbind();
-        assert(file1.get_name() == "");
-#if defined(__unix__)
-        file1.bind("./hello/world");
-        assert(file1.get_path() == "./hello/world");
-#elif defined(_MSC_VER)
-        file1.bind(".\\hello\\world");
-        assert(file1.get_path() == ".\\hello\\world");
-#endif
-        cout << file1.get_absolute_path() << endl;
-        cout << file2.get_absolute_path() << endl;
-        cout << File::get_time_str(file2.last_modified()) << endl;
-        cout << file1.get_size() << endl;
-        cout << file2.get_size() << endl;
-        assert(!file1.can_read());
-        assert(!file1.can_write());
-        assert(!file1.can_execute());
-        assert(file2.can_read());
-        assert(file2.can_write());
-        assert(File(file2.get_parent()).can_execute());
-        std::vector<string> v = file1.list();
-        for (const auto &s : v)
-                cout << s << endl;
+        File f4{s4};
+        assert(f4.create_new_file());
+        cout << "Expected: information of " << s4 << endl;
+        ls(s4);
         cout << endl;
-        v = file2.list();
-        for (const auto &s : v)
-                cout << s << endl;
+
+        assert(f4.remove());
+        cout << "Expected: no file named " << s4 << endl;
+        ls(s4);
         cout << endl;
-        v = File(file2.get_parent()).list();
-        for (const auto &s : v)
-                cout << s << endl;
-        cout << endl;
-        assert(file1.get_extension() == "");
-        assert(file2.get_extension() == "cpp");
-        File new_file{"./hello"};
-        assert(new_file.create_new_file());
+
+        f4.unbind();
+
 #if defined(__unix__)
-        system("ls -l");
-#elif defined(_MSC_VER)
-        system("dir");
-#endif
-        assert(new_file.remove());
-#if defined(__unix__)
-        system("ls -l");
-#elif defined(_MSC_VER)
-        system("dir");
-#endif
-        new_file.unbind();
-        string s;
-#if defined(__unix__)
-        s = "./a";
+        s4 = "./a";
 #elif defined(_MSC_VER)
         s = ".\\a";
 #endif
-        new_file.bind(s);
-        assert(new_file.mkdir());
+
+        f4.bind(s4);
+        assert(f4.mkdir());
+        cout << "Expected: information of " << s4 << endl;
+        ls(s4);
+        cout << endl;
+
 #if defined(__unix__)
-        system("ls -l");
-#elif defined(_MSC_VER)
-        system("dir");
-#endif
-#if defined(__unix__)
-        s = "./b";
+        s4 = "./b";
 #elif defined(_MSC_VER)
         s = ".\\b";
 #endif
-        assert(new_file.move(s));
-        assert(new_file.get_path() == s);
-#if defined(__unix__)
-        system("ls -l");
-#elif defined(_MSC_VER)
-        system("dir");
-#endif
-        assert(new_file.remove());
-#if defined(__unix__)
-        system("ls -l");
-#elif defined(_MSC_VER)
-        system("dir");
-#endif
+        assert(f4.move(s4));
+        assert(f4.get_path() == s4);
+        cout << "Expected: information of " << s4 << endl;
+        ls(s4);
+        cout << endl;
+
+        assert(f4.remove());
+        cout << "Expected: no file named " << s4 << endl;
+        ls(s4);
+        cout << endl;
 
         return 0;
 }
